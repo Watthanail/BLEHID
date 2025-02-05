714abb86f8aa3dba06546997ea99d77e417dabdc
watthanail <watthanai.l@ku.th>
2025-02-05 21:57:47 +0700

    update BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,42 @@
 #include "Arduino.h"
 #include <BleKeyboard.h>
 
-BleKeyboard bleKeyboard;
+BleKeyboard bleKeyboard("Mini01_BLE");
 
-#define RXD2 16
-#define TXD2 17
+#define RXD2 16 //16
+#define TXD2 17  //17
+#define BLE_STATUS_PIN 32
 
 QueueHandle_t weightQueue = NULL; 
 String buffer = ""; 
 
 // BLE task to send data
+bool wasConnected = false;
 void BLE_task(void *pvParameter) {
-  bleKeyboard.begin();
+    bleKeyboard.begin();
 
-  while (true) {
-    if (bleKeyboard.isConnected()) {
-      String receivedValue;
-      if (xQueueReceive(weightQueue, &receivedValue, portMAX_DELAY) == pdTRUE) {
-        bleKeyboard.print(receivedValue);  // Send via BLE
-      }
+    while (true) {
+        if (bleKeyboard.isConnected()) {
+            if (!wasConnected) {
+                Serial.println("BLE connected.");
+                wasConnected = true;
+                digitalWrite(BLE_STATUS_PIN,HIGH);
+            }
+
+            String receivedValue;
+            if (xQueueReceive(weightQueue, &receivedValue, portMAX_DELAY) == pdTRUE) {
+                Serial.println("Sending via BLE: " + receivedValue);
+                bleKeyboard.print(receivedValue);  // Send via BLE
+            }
+        } else {
+            if (wasConnected) {
+                Serial.println("BLE disconnected.");
+                wasConnected = false;
+                digitalWrite(BLE_STATUS_PIN,LOW);
+            }
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS); 
     }
-    vTaskDelay(10 / portTICK_PERIOD_MS); 
-  }
 }
 
 void setup() {
@@ -29,43 +44,140 @@ void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 
+  pinMode(BLE_STATUS_PIN,OUTPUT);
+  digitalWrite(BLE_STATUS_PIN,LOW);
 
-  weightQueue = xQueueCreate(32, sizeof(String));  
+
   
+    // Start BLE keyboard
+    // bleKeyboard.begin();
+    // Serial.println("BLE Keyboard initialized with security settings.");
 
-  xTaskCreatePinnedToCore(
-      BLE_task,           
-      "BLE_keyboard",     
-      4096,               
-      NULL,               
-      2,                  
-      NULL,               
-      0                   
-  );
+    weightQueue = xQueueCreate(32, sizeof(String));  
+
+    xTaskCreatePinnedToCore(
+        BLE_task,           // BLE task
+        "BLE_keyboard",     // Task name
+        4096,               // Stack size
+        NULL,               // Parameter
+        2,                  // Priority
+        NULL,               // Task handle
+        0                   // Core ID
+    );
 }
 
 void loop() {
-
-  if (Serial2.available()) {
-    char receivedChar = Serial2.read(); 
-
-
-    if (receivedChar != '\r' && receivedChar != '\n') {
-      buffer += receivedChar; 
-
-      if (buffer.startsWith("ST,GS,") && buffer.length() >= 16) {
-
-        Serial.println(buffer + " Length: " + String(buffer.length()));
-        String value = buffer.substring(7,14);  
-        buffer = "";  
-
+  // if (Serial2.available()) {
+  while (Serial2.available()) {
+    uint32_t decimalValue = Serial2.read();
+    // Serial.println(decimalValue);
+    if (decimalValue != 10 && decimalValue != 13) {  // Ignore newlines
+      char asciiChar = (char)decimalValue;
+      buffer += asciiChar;
+      Serial.println(buffer + " length: " + String(buffer.length()));
+      if (buffer.startsWith("ST,GS,") && buffer.length() >= 17) {
+        Serial.println(buffer + " length: " + String(buffer.length()));
+        String value = buffer.substring(7, 14);
+        buffer = "";
         Serial.println("Value: " + value);
 
+        // Send to BLE task
         xQueueSend(weightQueue, &value, portMAX_DELAY);  
       }
-
     }
   }
 
   delay(100); 
 }
+
+// #include "Arduino.h"
+// #include <BleKeyboard.h>
+// #include <HardwareSerial.h>
+
+// BleKeyboard bleKeyboard("Mini01_BLE");
+
+// #define RXD2   (20)
+// #define TXD2   (21)
+
+// HardwareSerial MySerial(0);
+
+// QueueHandle_t weightQueue = NULL; 
+// String buffer = ""; 
+
+// // BLE task to send data
+// bool wasConnected = false;
+// void BLE_task(void *pvParameter) {
+//     bleKeyboard.begin();
+
+//     while (true) {
+//         if (bleKeyboard.isConnected()) {
+//             if (!wasConnected) {
+//                 Serial.println("BLE connected.");
+//                 wasConnected = true;
+//             }
+
+//             String receivedValue;
+//             if (xQueueReceive(weightQueue, &receivedValue, portMAX_DELAY) == pdTRUE) {
+//                 Serial.println("Sending via BLE: " + receivedValue);
+//                 bleKeyboard.print(receivedValue);  // Send via BLE
+//             }
+//         } else {
+//             if (wasConnected) {
+//                 Serial.println("BLE disconnected.");
+//                 wasConnected = false;
+//             }
+//         }
+//         vTaskDelay(10 / portTICK_PERIOD_MS); 
+//     }
+// }
+
+// void setup() {
+
+//   Serial.begin(115200);
+//   MySerial.begin( 9600, SERIAL_8N1, RXD2, TXD2 );
+
+
+  
+//     // Start BLE keyboard
+//     // bleKeyboard.begin();
+//     // Serial.println("BLE Keyboard initialized with security settings.");
+
+//     weightQueue = xQueueCreate(32, sizeof(String));  
+
+//     xTaskCreatePinnedToCore(
+//         BLE_task,           // BLE task
+//         "BLE_keyboard",     // Task name
+//         4096,               // Stack size
+//         NULL,               // Parameter
+//         2,                  // Priority
+//         NULL,               // Task handle
+//         0                   // Core ID
+//     );
+// }
+
+// void loop() {
+//   // if (Serial2.available()) {
+//   while (MySerial.available()) {
+//     uint32_t decimalValue = MySerial.read();
+//     // Serial.println(decimalValue);
+//     if (decimalValue != 10 && decimalValue != 13) {  // Ignore newlines
+//       char asciiChar = (char)decimalValue;
+//       buffer += asciiChar;
+//       Serial.println(buffer + " length: " + String(buffer.length()));
+//       if (buffer.startsWith("ST,GS,") && buffer.length() >= 17) {
+//         Serial.println(buffer + " length: " + String(buffer.length()));
+//         String value = buffer.substring(7, 14);
+//         buffer = "";
+//         Serial.println("Value: " + value);
+
+//         // Send to BLE task
+//         xQueueSend(weightQueue, &value, portMAX_DELAY);  
+//       }
+//     }
+//   }
+
+//   delay(100); 
+// }
+
+
+
